598b286d379d28628a7757a8f06263006f59470a
joshmoisan <josh.moisan@hotmail.fr>
2022-11-14 15:26:43 -0500

    Done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require 'open-uri'
+
 class GamesController < ApplicationController
 
   def new
@@ -6,8 +9,27 @@ class GamesController < ApplicationController
 
   end
 
-  def scores
+  def score
+    @letters = params[:letters]
+    @user_input = params[:input]
 
+    @arr = @letters.split(" ")
+
+    @arr.each do |letter|
+      if @user_input.include? (letter)
+        @answer = "Sorry but #{@user_input} cant be build out of #{letter}"
+      elsif parse(@user_input) == false
+        @answer = "Sorry but #{@user_input} does not seem to be a valid English word..."
+      else
+        @answer = "Congratulation! #{@user_input} is a valid English word!"
+      end
+    end
   end
 
+  def parse(keyword)
+    url = "https://wagon-dictionary.herokuapp.com/#{keyword}"
+    file = URI.open(url).string
+    data_hash = JSON.parse(file)
+    data_hash["found"]
+  end
 end
